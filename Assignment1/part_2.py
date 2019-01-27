f5caea206edc90b78719e0ea1d8d520cd5b3629f
Your Name <you@example.com>
2019-01-26 17:25:04 -0800

    Fixed indexing in ./part_2.py

--- Assignment1/part_2.py
+++ Assignment1/part_2.py
@@ -6,30 +6,31 @@ from fnv import *
 import uuid
 
 LARGE_PRIME = 15373875993579943603
-RANDOM_NUM_DICT = {} #6*14*2 random numbers for the hash functions
+RANDOM_NUM_DICT = {}  # 6*14*2 random numbers for the hash functions
 B = 6
 S = 14
+
 
 def random_number():
     rand64 = uuid.uuid4().int & (1 << 64) - 1
     return rand64
 
-def generate_hash(a,b,word_64):
 
+def generate_hash(a, b, word_64):
     return (a * word_64 + b) % LARGE_PRIME
 
 
-
 def build_hash_sig(sig_num, words):
-    hash_sig = tuple([get_min_hash(sig_num, j+1, words) for j in range(B)])
+    hash_sig = tuple([get_min_hash(sig_num, j, words) for j in range(B)])
 
     return hash_sig
+
 
 def get_min_hash(sig_num, min_hash_num, words):
     hash_codes = []
 
-    a = RANDOM_NUM_DICT[(sig_num,min_hash_num,'a')]
-    b = RANDOM_NUM_DICT[(sig_num,min_hash_num,'b')]
+    a = RANDOM_NUM_DICT[(sig_num, min_hash_num, 'a')]
+    b = RANDOM_NUM_DICT[(sig_num, min_hash_num, 'b')]
 
     for word in words:
         word_encode = word.encode('utf-8')
@@ -40,18 +41,36 @@ def get_min_hash(sig_num, min_hash_num, words):
 
     return min(hash_codes)
 
+
 def main():
     f = open('./question_150k.tsv', encoding='utf8')
 
-    #make ditionary of random values for the 14*6 hash functions used in this algorithm
+    # make ditionary of random values for the 14*6 hash functions used in this algorithm
     for i in range(S):
         for j in range(B):
-            RANDOM_NUM_DICT[(i + 1, j + 1,'a')] = random_number()
-            RANDOM_NUM_DICT[(i + 1, j + 1, 'b')] = random_number()
+            RANDOM_NUM_DICT[(i, j, 'a')] = random_number()
+            RANDOM_NUM_DICT[(i, j, 'b')] = random_number()
 
-    #print(RANDOM_NUM_DICT)
+    # initialize 14 hash tables
+    '''
+            IMPORTANT: THERE HAS TO BE A BETTER WAY TO DO THIS
+    '''
+    hash_table_1 = {}
+    hash_table_2 = {}
+    hash_table_3 = {}
+    hash_table_4 = {}
+    hash_table_5 = {}
+    hash_table_6 = {}
+    hash_table_7 = {}
+    hash_table_8 = {}
+    hash_table_9 = {}
+    hash_table_10 = {}
+    hash_table_11 = {}
+    hash_table_12 = {}
+    hash_table_13 = {}
+    hash_table_14 = {}
 
-    #list of lines with the end "\n" stripped off
+    # list of lines with the end "\n" stripped off
     lines = [line.rstrip('\n') for line in f]
 
     # questions start on line 2
@@ -60,24 +79,25 @@ def main():
             (qid, question) = i.split('\t')
             # express words in question as a set, to remove duplicates
             words = set(question.strip().split(' '))
-            D = [build_hash_sig(j + 1, words) for j in range(S)]
+
+            D = [build_hash_sig(j, words) for j in range(S)]
 
             if int(qid) % 500 == 1 or int(qid) % 500 == 0:
                 print(qid)
             '''
             COMPARE D (say D_1) for "question" to every other questions' D (say D_2).
             As long as D_1[i] = D_2[i] for some i, then they are considered "similar".
-            
+
             To record this, we will create a new dictionary, say sim_dict, where we index into
             the dictionary by qid and the value of the dictionary are the questions similar to the 
             question with that qid.
-            
-            '''
 
+            '''
 
 
 if __name__ == '__main__':
     main()
+
 
 
 
