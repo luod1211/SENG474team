0fe2c5f592c2979592822b20d18e7cdef5cdc49c
Your Name <you@example.com>
2019-03-23 12:05:57 -0700

    almost done preprocessing

--- Assignment4/question1.py
+++ Assignment4/question1.py
@@ -10,15 +10,22 @@ import numpy as np
 import time
 
 
-def preprocess(lines):
 
-    # reindex the user_ids from 0 to n-1
-    users_idx = {}
-    # reindex the nove_ids from 0 to m-1
-    movies_idx = {}
+
+def preprocess(lines,N):
+
+    # reindex the user_ids from 0 to n-1. The key is the idx
+    find_user_id = {}
+    # reindex the user_ids from 0 to n-1. The key is the user_id
+    find_user_idx = {}
+    # reindex the movie_ids from 0 to m-1. The key is the idx
+    find_movie_id = {}
+    # reindex the movie_ids from 0 to m-1. The key is the movie_id
+    find_movie_idx = {}
+
     # for each user we have a dictionary storing the users' movies and
     # associated ratings
-    users_list = []
+    users_list = [{}]*N
 
     user_counter = 0
     movie_counter = 0
@@ -26,12 +33,14 @@ def preprocess(lines):
     for line in lines:
         if len(line.split('\t')) == 4:
             (user_id, movie_id, rating, _) = tuple(map(int, line.split('\t')))
-            if user_id not in users_idx.values():
-                users_idx[user_counter] = user_id
+            if user_id not in find_user_idx.keys():
+                find_user_id[user_counter] = user_id
+                find_user_idx[user_id] = user_counter
                 user_counter += 1
 
-            if movie_id not in movies_idx.values():
-                movies_idx[movie_counter] = movie_id
+            if movie_id not in find_movie_idx.keys():
+                find_movie_id[movie_counter] = movie_id
+                find_movie_idx[movie_id] = movie_counter
                 movie_counter += 1
 
         if counter % 10000 == 0:
@@ -39,11 +48,11 @@ def preprocess(lines):
 
         counter += 1
 
-    print("Number of users:", user_counter)
-    print("Numbers of movies:", movie_counter)
+    print("Number of users:", len(find_user_id.keys()), len(find_user_idx.keys()))
+    print("Numbers of movies:", len(find_movie_id.keys()), len(find_movie_idx.keys()))
 
 
-    return users_idx, movies_idx, users_list
+    return find_user_id,find_user_idx, find_movie_id, find_movie_idx, users_list
 
 
 def main():
@@ -53,9 +62,11 @@ def main():
     # list of lines of the u.data input file
     lines = [line.rstrip('\n') for line in f]
 
+    N = 943
+    M = 1682
+    D = 20
 
-
-    preprocess(lines)
+    find_user_id, find_user_idx, find_movie_id, find_movie_idx, users_list = preprocess(lines, N)
 
     f.close()
     outputU.close()
